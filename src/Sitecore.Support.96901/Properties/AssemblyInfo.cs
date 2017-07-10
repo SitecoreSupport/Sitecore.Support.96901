eda76f3e7f654379fa4573d0c594296222e4cb2c
DK\psk <psk@sitecore.net>
2017-07-10 14:43:10 -0400

    Add assembly information

--- src/Sitecore.Support.96901/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.96901/Properties/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.96901")]
 [assembly: AssemblyProduct("Sitecore.Support.96901")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("When non-admin users search in the content tree, a null reference exception could occur")]
